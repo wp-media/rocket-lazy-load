7577c86df381059c2d8c7842b676b51c57451611
Rémi Perona <remperona@gmail.com>
2017-09-01 15:18:25 -0400

    fix CS

--- 3rd-party/amp.php
+++ 3rd-party/amp.php
@@ -1,5 +1,5 @@
 <?php
-defined( 'ABSPATH' ) ||	die( 'Cheatin\' uh?' );
+defined( 'ABSPATH' ) || die( 'Cheatin\' uh?' );
 
 /**
  * Removes LazyLoad when on an AMP version of a post with the AMP for WordPress plugin from Auttomatic
