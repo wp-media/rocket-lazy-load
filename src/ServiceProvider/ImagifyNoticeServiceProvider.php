6d8c5315f734481d279225b71884dc1dd64096c3
Rémy Perona <remperona@gmail.com>
2018-12-11 12:26:05 -0500

    Add imagify notice classes and view

---
+++ src/ServiceProvider/ImagifyNoticeServiceProvider.php
@@ -0,0 +1,39 @@
+<?php
+namespace RocketLazyLoadPlugin\ServiceProvider;
+
+use League\Container\ServiceProvider\AbstractServiceProvider;
+
+/**
+ * Adds the Imagify notice to the container
+ *
+ * @since 2.0
+ * @author Remy Perona
+ */
+class ImagifyNoticeServiceProvider extends AbstractServiceProvider
+{
+    /**
+     * Data provided by the service provider
+     *
+     * @since 2.0
+     * @author Remy Perona
+     *
+     * @var array
+     */
+    protected $provides = [
+        'RocketLazyLoadPlugin\Admin\ImagifyNotice',
+    ];
+
+    /**
+     * Registers the Imagify notice in the container
+     *
+     * @since 2.0
+     * @author Remy Perona
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->getContainer()->add('RocketLazyLoadPlugin\Admin\ImagifyNotice')
+             ->withArgument($this->getContainer()->get('template_path'));
+    }
+}
