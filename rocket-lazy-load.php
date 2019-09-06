3b1bf95869d3abb052376203d672867b48ff0c62
Remy Perona <remy@wp-rocket.me>
2019-09-06 15:49:09 -0400

    update version to 2.3.1

--- rocket-lazy-load.php
+++ rocket-lazy-load.php
@@ -3,7 +3,7 @@
  * Plugin Name: Lazy Load by WP Rocket
  * Plugin URI: http://wordpress.org/plugins/rocket-lazy-load/
  * Description: The tiny Lazy Load script for WordPress without jQuery or others libraries.
- * Version: 2.3
+ * Version: 2.3.1
  * Author: WP Rocket
  * Author URI: https://wp-rocket.me
  * Text Domain: rocket-lazy-load
@@ -29,7 +29,7 @@
 
 defined('ABSPATH') || die('Cheatin\' uh?');
 
-define('ROCKET_LL_VERSION', '2.3');
+define('ROCKET_LL_VERSION', '2.3.1');
 define('ROCKET_LL_WP_VERSION', '4.7');
 define('ROCKET_LL_PHP_VERSION', '5.6');
 define('ROCKET_LL_BASENAME', plugin_basename(__FILE__));
