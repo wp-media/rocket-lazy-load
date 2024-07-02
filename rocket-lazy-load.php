cb6a3c97fde7f2cc35c5162bdba946bea96e29cd
Nico Mollet <nico.mollet@gmail.com>
2024-07-02 12:27:20 +0200

    Updated copyright date

--- rocket-lazy-load.php
+++ rocket-lazy-load.php
@@ -13,7 +13,7 @@
  *
  * @package RocketLazyloadPlugin
  *
- * Copyright 2015-2019 WP Media
+ * Copyright 2015-2024 WP Media
  *
  * This program is free software; you can redistribute it and/or modify
  *     it under the terms of the GNU General Public License as published by
