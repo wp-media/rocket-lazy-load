36c3f93fd2994f3b69eb31d21ab14a51dfb543e1
Rémy Perona <remperona@gmail.com>
2026-04-16 11:45:37 -0400

    remove unused providers

--- configs/providers.php
+++ configs/providers.php
@@ -3,8 +3,6 @@
 defined( 'ABSPATH' ) || exit;
 
 return [
-	\RocketLazyLoadPlugin\Dependencies\LaunchpadFrameworkOptions\ServiceProvider::class,
-	\RocketLazyLoadPlugin\ServiceProvider\SubscribersServiceProvider::class,
 	\RocketLazyLoadPlugin\ServiceProvider\AdminServiceProvider::class,
 	\RocketLazyLoadPlugin\ServiceProvider\ImagifyNoticeServiceProvider::class,
 	\RocketLazyLoadPlugin\ServiceProvider\LazyloadServiceProvider::class,
